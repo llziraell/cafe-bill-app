cee0b1f455af8f44f30dcc6e9deae17587a5bf35
llziraell <alex.sokolovva@gmail.com>
2023-09-08 18:24:10 +0300

    add main.js & README.md

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { BootstrapVueNext } from 'bootstrap-vue-next'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(BootstrapVueNext)
+
+app.mount('#app')
+
+
