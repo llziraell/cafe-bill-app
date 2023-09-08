28b8640346bbbf9f42c50923feeda14f886e68d0
llziraell <alex.sokolovva@gmail.com>
2023-09-08 19:37:18 +0300

    add result view

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import FormBlock from '@/components/FormBlock.vue'
+import ResultForm from '@/components/ResultForm.vue'
+
+</script>
+
+<template>
+    <form-block>
+        <template #header style = "justify-content: flex-end !important;">
+            <h2>...час расплаты</h2>
+        </template>
+        <hr>
+        <template #body>
+        </template>
+    </form-block>
+</template>
