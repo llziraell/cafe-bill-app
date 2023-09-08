07232225802cd1e18b4999004cd828b023233895
llziraell <alex.sokolovva@gmail.com>
2023-09-08 19:14:23 +0300

    add methods for dishes in pinia store

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -25,5 +25,48 @@ export const useUserStore = defineStore("user", {
         deleteUserForm(id) {
             this.users = this.users.filter((el) => el.id !== id)
         },
+        addDishForm(users) {
+            let newDish = {
+                id: Date.now(),
+                dish: "",
+                cost: "",
+                whoPay: "",
+                whoEat: [],
+                users: [...users],
+                selectAllChecked: false,
+                isValid: false,
+            }
+            this.dishes.push(newDish)
+        },
+        deleteDishForm(id) {
+            this.dishes = this.dishes.filter((el) => el.id !== id)
+        },
+        addNewDish(id, dish) {
+            this.dishes.map((el) => {
+                if (el.id === id) {
+                    el.dish = dish
+                }
+            })
+        },
+        addNewCost(id, cost) {
+            this.dishes.map((el) => {
+                if (el.id === id) {
+                    el.cost = cost
+                }
+            })
+        },
+        selectWhoPay(id, selected) {
+            this.dishes.map((el) => {
+                if (el.id === id) {
+                    el.whoPay = selected.name
+                }
+            })
+        },
+        selectAllUsers() {
+            const users = this.users.map((el) => el.name)
+            for (let item of this.dishes) {
+                item.users = users
+            }
+        },
     },
 })
